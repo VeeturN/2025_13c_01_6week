898c30c30b472a37d9bcdc3cd11b3f4c0a07313a
s32314 <s32314@pjwstk.edu.pl>
2025-10-12 22:43:43 +0200

    EndPoind
    -przeskakuje na inną scene (wybraną)
    -brak fajnego sprite
    -brak dodatkwoego warunku

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad; // nazwa sceny ustawiana w Inspectorze
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //tutaj zrobimy drugiego if na warunken np score
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
