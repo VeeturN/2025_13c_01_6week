11d9fd76eccb743a074f0787bad2591331f46ed0
Dawid Juszyński <dawid.games21@gmail.com>
2025-10-14 02:12:07 +0200

    Przeciwnik: patroluje, goni, skacze, atakuje, część animacji zrobiona.

--- Assets/Scripts/Enemy1.cs
+++ Assets/Scripts/Enemy1.cs
@@ -5,7 +5,6 @@ public class Enemy1 : MonoBehaviour
     [SerializeField] private Transform _patrolPointA;
     [SerializeField] private Transform patrolPointB;
     [SerializeField] private float _speed = 2f;
-    [SerializeField] private float _detectionRange = 0f; // Zasięg wykrywania gracza
     private BasicPlayerMovment _player;
     private Rigidbody2D _rb;
     private bool _movingToB = true;
@@ -13,17 +12,14 @@ public class Enemy1 : MonoBehaviour
     public bool IsPlayerInAttackRange { get; set; } = false;
     private float _hitTimer = 0f;
     private Animator _animator;
-
-    private float _attackAnimDuration = 0.5f; // czas trwania animacji ataku
-    private float _attackAnimTimer = 0f;
-    private float _attackAnimLeadTime = 0.3f; // ile wcześniej ma się zacząć animacja
-    [SerializeField] private float _attackInterval = 1f; //predkosc ataku
+    
+    [SerializeField] private float _attackInterval = 2f; //predkosc ataku
 
     private void Awake()
     {
         _player = FindObjectOfType<BasicPlayerMovment>();
         _rb = GetComponent<Rigidbody2D>();
-        _rb.freezeRotation = true; // Blokada rotacji przeciwnika
+        _rb.freezeRotation = true;  // Blokada rotacji przeciwnika
         _animator = GetComponent<Animator>();
     }
 
@@ -32,6 +28,14 @@ public class Enemy1 : MonoBehaviour
         if (_player && IsPlayerInDetectionRange())
         {
             MoveTowardsPlayer();
+            
+            if (_player.transform.position.y > transform.position.y)
+            {
+                if (Random.value < 0.025f) // 60% szans na skok
+                {
+                    Jump();
+                }
+            }
         }
         else
         {
@@ -41,32 +45,12 @@ public class Enemy1 : MonoBehaviour
         if (IsPlayerInAttackRange)
         {
             _hitTimer += Time.fixedDeltaTime;
-
-            // Animacja startuje wcześniej
-            if (_hitTimer >= _attackInterval - _attackAnimLeadTime && _attackAnimTimer <= 0f)
+            if (_hitTimer > _attackInterval)
             {
-
                 _animator.SetBool("isAttacking", true);
-                _attackAnimTimer = _attackAnimDuration + _attackAnimLeadTime;
-            }
-
-            // Napis HIT pojawia się później
-            if (_hitTimer >= _attackInterval)
-            {
-                Debug.Log("HIT");
                 _hitTimer = 0f;
             }
-        }
-
-        // if (_attackAnimTimer > 0f)
-        // {
-        //     _attackAnimTimer -= Time.fixedDeltaTime;
-        //     if (_attackAnimTimer <= 0f)
-        //          _animator.SetBool("isAttacking", false);
-        // }
-        else if (!IsPlayerInAttackRange)
-        {
-             _animator.SetBool("isAttacking", false);
+            
         }
 
         if (!_animator.GetBool("isAttacking"))
@@ -89,15 +73,29 @@ public class Enemy1 : MonoBehaviour
     private bool IsPlayerInDetectionRange()
     {
         float playerX = _player.transform.position.x;
-        return playerX >= Mathf.Min(_patrolPointA.position.x, patrolPointB.position.x) && //-detectionRange 
-               playerX <= Mathf.Max(_patrolPointA.position.x,
-                   patrolPointB.position.x); //+detectionRange XD Nawet jak jest 0 to szuka dalej beka
+        float playerY = _player.transform.position.y;
+        float enemyY = transform.position.y;
+
+        bool inXRange = playerX >= Mathf.Min(_patrolPointA.position.x, patrolPointB.position.x) &&
+                        playerX <= Mathf.Max(_patrolPointA.position.x, patrolPointB.position.x);
+
+        bool inYRange = Mathf.Abs(playerY - enemyY) <= 10f;
+
+        return inXRange && inYRange;
     }
 
     private void MoveTowardsPlayer()
     {
-        float direction = Mathf.Sign(_player.transform.position.x - transform.position.x);
-        _rb.velocity = new Vector2(direction * _speed, _rb.velocity.y);
+        float deltaX = _player.transform.position.x - transform.position.x;
+        if (Mathf.Abs(deltaX) > 0.2f) // tolerancja, gdy gracz jest "nad" przeciwnikiem
+        {
+            float direction = Mathf.Sign(deltaX);
+            _rb.velocity = new Vector2(direction * _speed, _rb.velocity.y);
+        }
+        else
+        {
+            _rb.velocity = new Vector2(0, _rb.velocity.y); // zatrzymaj ruch w osi x
+        }
     }
 
     private void Patrol()
@@ -112,16 +110,36 @@ public class Enemy1 : MonoBehaviour
             _movingToB = !_movingToB;
         }
     }
+    
+    private void Jump()
+    {
+        if (Mathf.Abs(_rb.velocity.y) < 0.01f) // tylko gdy stoi na ziemi
+        {
+            float jumpForce = Random.Range(3f, 6f);
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
 
     public void madeAttack()
     {
         _animator.SetBool("isAttacking", false);
+        Debug.Log("Koniec");
     }
 
     public void tryToHitPlayer()
     {
-        
-        
+        if (IsPlayerInAttackRange)
+        {
+            _player.hit();
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _movingToB=!_movingToB;
+        }
     }
 
 }
