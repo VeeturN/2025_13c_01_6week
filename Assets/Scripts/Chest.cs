e796bfb8026dbaec1ca1ccee5906398423a2eec6
User31181 <s31181@pjwstk.edu.pl>
2025-10-20 21:34:28 +0200

    Dodanie dropu ze skrzynki

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -6,14 +6,17 @@ using UnityEngine.UIElements;
 
 public class Chest : MonoBehaviour
 {
+    [SerializeField] private GameObject[] _itemsToDrop;
     private Transform _player; 
     private float _interactionDistance = 3f; 
     private BasicPlayerMovment _playerObj;
-    [SerializeField] private Animator _animator;
+    private Animator _animator;
     private bool _isOpened = false;
+
 
     private void Start()
     {
+        _animator = GetComponent<Animator>();
         GameEventSystem.OnChestOpen += OpenChest;
     }
     private void OnDestroy()
@@ -39,13 +42,34 @@ public class Chest : MonoBehaviour
             _animator.SetBool("IsOpen", true);
             Inventory.SetKeysCollected(Inventory.GetKeysCollected() - 1);
             _isOpened = true;
-            StartCoroutine(DestroyAfterAnimation());
         }
     }
 
-    private IEnumerator DestroyAfterAnimation()
+    private void DestroyAfterAnimation()
     {
-        yield return new WaitForSeconds(_animator.GetCurrentAnimatorStateInfo(0).length);
+        StartCoroutine(WaitForToCollectCoroutine());
+    }
+
+    private IEnumerator WaitForToCollectCoroutine()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        float chestWidth = GetComponent<BoxCollider2D>().bounds.size.x;
+
+        Vector3 dropPos1 = transform.position + new Vector3(-chestWidth * 0.3f, 0, 0);
+        Vector3 dropPos2 = transform.position + new Vector3(chestWidth * 0.3f, 0, 0);
+
+        BoxCollider2D box1 = Instantiate(_itemsToDrop[UnityEngine.Random.Range(0, _itemsToDrop.Length)], dropPos1, Quaternion.identity).GetComponent<BoxCollider2D>();
+        BoxCollider2D box2 = Instantiate(_itemsToDrop[UnityEngine.Random.Range(0, _itemsToDrop.Length)], dropPos2, Quaternion.identity).GetComponent<BoxCollider2D>();
+
+        box1.enabled = false;
+        box2.enabled = false;
+
+        yield return new WaitForSeconds(1);
+
+        box1.enabled = true;
+        box2.enabled = true;
+
         Destroy(gameObject);
     }
 }
