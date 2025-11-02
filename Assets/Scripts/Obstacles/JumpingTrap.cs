8e1e3969c8a201398aef9b547d94c63528b7c2f1
User31181 <s31181@pjwstk.edu.pl>
2025-11-02 19:39:41 +0100

    Zmiana wartości prefabów

--- Assets/Scripts/Obstacles/JumpingTrap.cs
+++ Assets/Scripts/Obstacles/JumpingTrap.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JumpingTrap : MonoBehaviour
 {
-    private float impactSpeed = 10f;
+    [SerializeField] private float impactSpeed = 10f;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
