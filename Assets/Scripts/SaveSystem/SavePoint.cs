a5beda6b265d73d00cde516f7b1b641443cc827a
OlafPJATK <s32306@pjwstk.edu.pl>
2025-11-02 10:54:48 +0100

    zapis gry na fladze

---
+++ Assets/Scripts/SaveSystem/SavePoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class SavePoint : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        Debug.Log("ZAPIS GRY SIĘ WYKONAŁ");
+        SaveManager.SaveGameStateDataXML();
+        SaveManager.SaveLevelDataXML(SaveManager._currentLevelIndex);
+    }
+}
