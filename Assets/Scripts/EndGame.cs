f7d566c445208c48d735efe7c6fd792ed3a0e3c3
OlafPJATK <s32306@pjwstk.edu.pl>
2025-11-04 11:05:11 +0100

    dla dawida

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class EndGame : MonoBehaviour
 {
-    [SerializeField] private string sceneToLoad; // nazwa sceny ustawiana w Inspectorze
+    [SerializeField] private string sceneToLoad; 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            //tutaj zrobimy drugiego if na warunken np score
+            //jesli odblokujemy nowy poziom ale jeszce bez sprawdzenia
+            //i jednoczesnie zapisz poziom
+            SaveManager.SaveGameStateDataXML();
+            SaveManager.SaveLevelDataXML(SaveManager._currentLevelIndex, other.transform.position);
+            
+            int currSlot = SaveManager.GetCurrentSlot();
+            int juzOdblowkowane = SaveManager.GetCurrentUnlockedLevels(currSlot);
+            
+            SaveManager.SaveCurrentUnlockedLevels(currSlot, juzOdblowkowane+1);
             SceneManager.LoadScene(sceneToLoad);
         }
     }
