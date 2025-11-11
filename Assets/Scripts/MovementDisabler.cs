08e5f325430a1b580fac65bb73a56c2014eb3c2b
s32314 <s32314@pjwstk.edu.pl>
2025-11-11 17:33:20 +0100

    zmiany na poziomach zmiany i nowe prefaby

---
+++ Assets/Scripts/MovementDisabler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementDisabler : MonoBehaviour
+{
+    [SerializeField] private float disableTime = 3f; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        BasicPlayerMovment player = other.GetComponent<BasicPlayerMovment>();
+        if (player != null)
+        {
+            StartCoroutine(DisableMovement(player));
+        }
+    }
+
+    private IEnumerator DisableMovement(BasicPlayerMovment player)
+    {
+        player.StopMovement();
+        yield return new WaitForSeconds(disableTime);
+        player.ResumeMovement();
+    }
+}
